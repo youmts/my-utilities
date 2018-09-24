0c266b9a9ff1988140478428adfaef35aac81e25
youmts <anata0814@gmail.com>
2018-09-24 10:39:14 +0900

    Add initial PureClassLibrary

---
+++ PureClassLibrary/StringSplitter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureClassLibrary
+{
+    public class StringSplitter
+    {
+        public IEnumerable<string> Split(string text, int byteLength)
+        {
+            return new [] {text};
+        }
+    }
+}
---
+++ PureClassLibraryTest/StringSplitterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PureClassLibrary;
+using Xunit;
+
+namespace PureClassLibraryTest
+{
+    public class StringSplitterTest
+    {
+        [Theory]
+        [InlineData("", 10, "")]
+        void SplitTest(string input, int byteLength, string expected1, string expected2 = null)
+        {
+            var ss = new StringSplitter();
+            var expected = (new[] {expected1, expected2}).Where(x => x != null);
+            ss.Split(input, byteLength).Is(expected);
+        }
+    }
+}
